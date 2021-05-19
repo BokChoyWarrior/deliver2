28068d2361e7c4a6a3228cc5201420be3d9d6932
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2021-05-19 20:09:20 +0100

    implement basic api function

--- routes/api/shops.js
+++ routes/api/shops.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var Shops = require('../../models/shop');
 var Items = require('../../models/items');
 
-router.get('/shops', function (req, res) {
-    // var shops = await Shops.find();
-    // res.json(shops);
-    res.send("Hello")
+router.get('/shops', async function (req, res) {
+    var shops = await Shops.find();
+    res.json(shops);
+    // res.send("Hello")
 })
 
 module.exports = router;
