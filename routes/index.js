ccac6515c94842499fce47fc2941498c651c818b
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2021-05-30 15:42:32 +0100

    Remove stale code

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const { ensureAuthenticated } = require('../strategies/auth')
 const router = express.Router()
 const Shops = require('../models/shop')
 const Items = require('../models/items')
@@ -9,12 +8,6 @@ router.get('/', async function (req, res, next) {
   const items = await Items.find() // and all the items
   // to send the current user logged in to handlebars we just pass the req.user variable. that's where the user information is attached to.
   res.render('index', { title: 'Delivery App', user: req.user, shops: shops, items: items })
-})
-
-router.all('/secret', ensureAuthenticated, function (req, res, next) {
-  console.log('Accessing the secret section ...')
-  res.render('index', { title: 'Not so secret!' })
-  // res.redirect('/'); // redirects back to '/' route defined above
 })
 
 module.exports = router
