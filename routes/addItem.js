954cf864a6c9e692737dd1de729101e299814b02
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2021-06-11 12:36:21 +0100

    Clean up addItem page JS and njk

--- routes/addItem.js
+++ routes/addItem.js
@@ -1,62 +1,34 @@
-var express = require('express');
-var router = express.Router();
-var User = require('../models/user');
-var Items = require('../models/items');
-const { ensureAuthenticated } = require('../strategies/auth');
-const { head } = require('./users');
+const express = require('express')
+const router = express.Router()
+const Items = require('../models/items')
+const { ensureAuthenticated } = require('../strategies/auth')
 
 // your code here
 
-router.get('/', ensureAuthenticated, async(req, res, next) => {
+router.route('/', ensureAuthenticated)
+  .get(async (req, res, next) => {
     if (req.user.type !== 1) {
-        res.redirect('/')
-    }else{
-        res.render('additem', { user: req.user }); 
+      res.redirect('/')
+    } else {
+      res.render('additem', { user: req.user })
     }
-    
-
-});
-router.post('/', ensureAuthenticated, function (req, res, next) {
-    if (req.user.type !== 1) {  
-        res.redirect('/users/login')
-        return
+  })
+  .post(async (req, res, next) => {
+    if (req.user.type !== 1) {
+      res.redirect('/users/login')
+      return
     }
 
-    var { item, itemdescription, price } = req.body
-    console.log(item, itemdescription, price)
-    var item = new Items({
-    name:item,
-    description:itemdescription,
-    price:price });
-    console.log(item);
-    item.save();
+    const { itemName, itemDescription, price } = req.body
+    const item = new Items({
+      name: itemName,
+      description: itemDescription,
+      price: price
+    })
+    console.log(item)
 
+    // item.save()
+    // redirect user to add another, or actually accept the item via react/jquery?
+  })
 
-
-    // for reference:
-
-    //Item name; item description and item price in PENCE! 
-    // ============================================================
-    // console.log(req.body);
-    // var { email, password, userType } = req.body;
-    // var type = 0;
-    // if (userType == "on") {
-    //     type = 1;
-    // }
-
-    // console.log(type);
-    // await bcrypt.genSalt(saltRounds, async function (err, salt) {
-    //     bcrypt.hash(password, salt, function (err, hash) {
-    //         var user = new User({
-    //             type: type,
-    //             email: email,
-    //             password: hash
-    //         });
-    //         user.save().then(data => {
-    //             console.log(data);
-    //             res.send({ msg: email + " has been registered" });
-    //         }).catch(err => console.log(err));
-    //     });
-    // });
-});
-module.exports=router
+module.exports = router
