36391c6d711dfca271dacef479b72fb184cd2dd9
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2021-06-17 18:20:05 +0100

    Add eslint for react components

--- public/react_components/ShopItems.js
+++ public/react_components/ShopItems.js
@@ -1,10 +1,10 @@
 const Item = (props) => {
-  const [itemInfo, setItemInfo] = React.useState({})
+  // const [itemInfo, setItemInfo] = React.useState({})
   const [quantity, setQuantity] = React.useState(props.quantity)
-  async function onClick(amount) {
-    const res = await axios.put(`/api/account/baskets/${props.shopId}`, {itemId: props.item._id, amount: amount})
+  async function onClick (amount) {
+    const res = await axios.put(`/api/account/baskets/${props.shopId}`, { itemId: props.item._id, amount: amount })
     const item = res.data.new_item
-    setQuantity((item == undefined ? 0 : item.quantity))
+    setQuantity((item === undefined ? 0 : item.quantity))
   }
   return (
     <div className="shop-item" id={props.item._id}>
@@ -25,7 +25,7 @@ const ShopItems = (props) => {
   const [items, setItems] = React.useState([])
   const [basket, setBasket] = React.useState(null)
   const shopId = localStorage.getItem('shop_id')
-    
+
   // Comment
   // only run after mounting
   React.useEffect(async () => {
@@ -44,14 +44,15 @@ const ShopItems = (props) => {
 
   return (
     items.map((item, _index) => {
-      var quantity = 0
+      let quantity = 0
       const index = basket.findIndex(x => x.item._id === item._id)
-      quantity = (index >=0) ? basket[index].quantity : 0
+      quantity = (index >= 0) ? basket[index].quantity : 0
       return (<Item key={item._id} item={item} basket={basket} shopId={shopId} quantity={quantity} />)
     }
     )
   )
 }
+
 const shopId = localStorage.getItem('shop_id')
 const element = document.querySelector('.shop-items')
 if (element !== null) {
